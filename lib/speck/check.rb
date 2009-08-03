038eed5d7651210d182a78d343147eee014abc40
elliottcable <git@elliottcable.name>
2009-08-03 01:31:29 -0800

    Checks work on subsequent lines

--- lib/speck/check.rb
+++ lib/speck/check.rb
@@ -27,7 +27,8 @@ class Speck
     end
     Speck.new :execute do
       Check.new(->{true}).execute.check
-      ->{ Check.new(->{false}).execute }.check_exception Speck::Exception::CheckFailed
+      ->{ Check.new(->{false}).execute }
+        .check_exception Speck::Exception::CheckFailed
       
       Check.new(->{"value"}).execute.check
       Check.new(->{2 * 2}).execute.check {|value| value == 4 }
