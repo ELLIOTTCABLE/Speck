dc950cb078c8597f09efe41e8429807df01d14f0
elliottcable <git@elliottcable.name>
2009-09-02 22:48:05 -0400

    Specking Check#initialize, not Class::new /-:

--- lib/speck/check.rb
+++ lib/speck/check.rb
@@ -31,7 +31,7 @@ class Speck
       @block = block
       @description = description
     end
-    Speck.new Check.method :new do
+    Speck.new Check.instance_method :initialize do
       my_lambda = ->{}
       Check.new(&my_lambda).check {|c| c.block == my_lambda }
       
